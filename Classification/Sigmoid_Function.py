b69f0a5217bceb7b7fcbececd856bb7d5f80ef1e
Nick Tanzola <53411237+NickTanzola@users.noreply.github.com>
2025-10-21 18:52:36 -0400

    Implement sigmoid function for logistic regression

--- Classification/Sigmoid_Function.py
+++ Classification/Sigmoid_Function.py
@@ -1,1 +1,6 @@
 #here is the implementation of the sigmoid function for logistic regression
+import numpy as np
+import math
+#z is the value of the model's prediction
+def sigmoid_function(z):
+  return 1 / (1 + math.exp(-z))
